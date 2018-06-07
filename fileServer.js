e10d9043d070943bf36f5843bfc5e43d4569b7b4
unknown <thunderblarg@gmail.com>
2018-06-06 23:11:31 -0700

    6/6/2018 Handler added to serve html, css, and png files

--- fileServer.js
+++ fileServer.js
@@ -6,31 +6,51 @@ var path = require("path");
 var listeningPort = 3000;
 var validTypes = [
      ".html",
-     ".css"
+     ".css",
+     ".png"
 ];
+var fileStream;
+//var indexPath;
 
 http.createServer(function(req, res){
      if(req.url === "/"){
           var indexPath = path.join(__dirname, "public", "index.html");
-          var fileStream = fs.createReadStream(indexPath, "UTF-8");
-
+          fileStream = fs.createReadStream(indexPath, "UTF-8");
           res.writeHead(200, {"Content-Type": "text/html"});
-
           fileStream.pipe(res);
-     } /*else if () {
+     } else if (validTypes.includes(req.url.substring(req.url.lastIndexOf(".")))){
           //TODO: Later down the line, make a handler for supported file types
-          var requestPath = path.join(__dirname, "public". req.url);
-          res.writeHead
-     }*/ else {
+               //Completed on 6/6/18
+          var requestPath = path.join(__dirname, "public", req.url);
+
+          switch ((req.url.substring(req.url.lastIndexOf(".")))) {
+               case ".html":
+                    fileStream = fs.createReadStream(requestPath, "UTF-8");
+                    res.writeHead(200, {"Content-Type": "text/html"});
+                    fileStream.pipe(res);
+                    break;
+               case ".css":
+                    fileStream = fs.createReadStream(requestPath, "UTF-8");
+                    res.writeHead(200, {"Content-Type": "text/css"});
+                    fileStream.pipe(res);
+                    break;
+               case ".png":
+                    fileStream = fs.createReadStream(requestPath);
+                    res.writeHead(200, {"Content-Type": "image/png"});
+                    fileStream.pipe(res);
+                    break;
+               default:
+                    break;
+          }
+     } else {
           res.writeHead(404, {"Content-Type": "text/plain"});
           res.end("404 File not fuck");
      }
-
-     //testing
-
 }).listen(listeningPort);
 
 console.log(`File server running on port ${listeningPort}`);
 
 //TODO: Later down the line, make a handler for supported file types;
 //else if (validTypes.contains(req.url.substring(lastIndexOf("."))));
+
+//var fileStream = fs.createReadStream(indexPath, "UTF-8");
